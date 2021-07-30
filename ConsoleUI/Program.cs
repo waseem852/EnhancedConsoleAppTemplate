a131bb11db1c88007bf1079e9d04088b0ce5029d
m.waseem <m.waseem@EHQ-WASEEM-L>
2021-07-30 19:29:42 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                //.Enrich.FromLogContext()
+                //.WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+
+            var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            svc.Run();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environments.Production}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    }
+}
---
+++ ConsoleUI/Services/GreetingService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleUI
+{
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> _log;
+        private readonly IConfiguration _config;
+
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+        public void Run()
+        {
+            var greetingMessage = _config.GetValue<string>("GreetingMessage");
+
+
+            _log.LogInformation("Hello {greetingMessage}", greetingMessage);
+
+        }
+    }
+}
